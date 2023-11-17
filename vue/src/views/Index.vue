8d280b87c80b97029e3c99cb77ec4afeea916d41
arminsb <76691001+arminsb@users.noreply.github.com>
2023-11-17 18:29:18 +0330

    project init, tailwind init

---
+++ vue/src/views/Index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1 class="text-3xl font-sans container mx-auto bg-primary px-4 py-4">This is create form</h1>
+  </div>
+</template>
---
+++ vue/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#4377FE'
+      }
+    },
+  },
+  plugins: [],
+}
+
